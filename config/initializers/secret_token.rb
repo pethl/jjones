4df4ab5c6c4f1480318d32d614db0c02e51aa923
pethl <admin@fencedweller.com>
2013-03-15 12:48:34 +0000

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+JJones::Application.config.secret_token = '53c878ca2f014c1e1d513a4e70ce352aceda041610c2625267a5a848fd65dc6629ee206ab155dcec23505723f9eaf7132de7c271f10b7215164ee0c6b5d8c68d'
