46665ba66aba7cabcdcb9de31a35fa9c5210e1bc
pethl <admin@fencedweller.com>
2013-03-26 13:32:51 +0000

    update ReadMe and final test bash heroku script

--- script/script_bash/git_heroku.sh
+++ script/script_bash/git_heroku.sh
@@ -1,22 +1,22 @@
 #!/bin/bash
-echo "Running git add"
+echo "----->Running git add"
 git add . && \
 git add -u && \
 
-echo "Running git status"
+echo "----->Running git status"
 git status
 
-echo -e "Enter desc: \c "
+echo -e "----->Enter desc: \c "
 read  desc
 
-echo "Running git commit"
+echo "----->Running git commit"
 git commit -m "$desc" && \
-echo "Git commit complete"
+echo "----->Git commit complete"
 
-echo "Running git push"
+echo "----->Running git push"
 git push
-echo "Git push complete"
+echo "----->Git push complete"
 
-echo "Running heroku push"
+echo "----->Running heroku push"
 git push heroku
-echo "Heroku push complete, script ended"
+echo "----->Heroku push complete, script ended"
