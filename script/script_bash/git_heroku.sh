a480c035b55573cfd2366f97ae6ca4e8a34a4373
pethl <admin@fencedweller.com>
2013-03-26 07:36:11 +0000

    bash git heroku files

--- script/script_bash/git_heroku.sh
+++ script/script_bash/git_heroku.sh
@@ -8,8 +8,15 @@ git status
 
 echo -e "Enter desc: \c "
 read  desc
-echo "Confirm desc: $desc"
 
 echo "Running git commit"
 git commit -m "$desc" && \
+echo "Git commit complete"
+
+echo "Running git push"
 git push
+echo "Git push complete"
+
+echo "Running heroku push"
+git push heroku
+echo "Heroku push complete, script ended"
---
+++ script/script_bash/just_git.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+echo "Running git add"
+git add . && \
+git add -u && \
+
+echo "Running git status"
+git status
+
+echo -e "Enter desc: \c "
+read  desc
+echo "Confirm desc: $desc"
+
+echo "Running git commit"
+git commit -m "$desc" && \
+git push
+echo "Git push complete, script ended"
